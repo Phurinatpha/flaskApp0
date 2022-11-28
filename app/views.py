98fce9e1d5c8f72843781aaed8f1dfead860d61e
phurinatpha <Phurinatpha>
2022-11-29 02:53:49 +0700

    serving JSON

--- app/views.py
+++ app/views.py
@@ -1,6 +1,20 @@
+from flask import jsonify
 from app import app
  
  
 @app.route('/')
 def home():
     return "Flask says 'Hello world!'"
+
+@app.route('/phonebook')
+def index():
+    return app.send_static_file('phonebook.html')
+
+@app.route("/api/data")
+def data():
+    # define some data
+    d = {
+        "Alice": "(708) 727-2377",
+        "Bob": "(305) 734-0429"
+    }
+    return jsonify(d)  # convert your data to JSON and return
