355be93953c62f64a5b5b372a1a58130c3cc15c9
Phurinatpha <phurinat_phanuphong@cmu.ac.th>
2022-12-05 19:02:06 +0700

    finish

--- app/views.py
+++ app/views.py
@@ -22,3 +22,8 @@ def data():
 @app.route('/lab02')
 def resume():
     return app.send_static_file('lab02_resume.html')
+
+
+@app.route('/lab')
+def resu():
+    return app.send_static_file('lab02_resume_m.html')
