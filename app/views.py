cea7fe6d39642b6f597e8884ceb825bfa2cc5412
Manging001 <eenaman001@gmail.com>
2022-12-13 13:24:32 +0700

    lab03(did'push) + lab04

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from flask import jsonify
+import datetime
+from flask import jsonify, render_template
 from app import app
  
  
@@ -10,13 +11,16 @@ def home():
 def index():
     return app.send_static_file('phonebook.html')
 
-@app.route("/api/data")
+@app.route('/api/data')
 def data():
     # define some data
     d = {
         "Alice": "(708) 727-2377",
         "Bob": "(305) 734-0429"
     }
+ 
+    app.logger.debug(str(len(d)) + " entries in phonebook")
+ 
     return jsonify(d)  # convert your data to JSON and return
 
 @app.route('/lab02')
@@ -24,6 +28,24 @@ def resume():
     return app.send_static_file('lab02_resume.html')
 
 
-@app.route('/lab')
-def resu():
-    return app.send_static_file('lab02_resume_m.html')
+@app.route('/lab03')
+def lab03_home():
+    return render_template('lab03/index.html',utc_dt=datetime.datetime.utcnow())
+
+@app.route('/lab03/about/')
+def lab03_about():
+    return render_template('lab03/about.html')
+
+
+@app.route('/lab03/comments/')
+def lab03_comments():
+    comments = ['This is the first comment.',
+                'This is the second comment.',
+                'This is the third comment.',
+                'This is the fourth comment.']
+ 
+    return render_template('lab03/comments.html', comments=comments)
+
+@app.route('/lab04')
+def leb04_bootstrap():
+    return app.send_static_file('lab04_bootstrap.html')
