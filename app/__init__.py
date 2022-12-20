51ca27e571dc99daceae41cc171617eeead1cf58
Phurinatpha <phurinat_phanuphong@cmu.ac.th>
2022-12-20 15:04:48 +0700

    lab05

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,5 @@ from flask import Flask
 app = Flask(__name__, static_folder='static')
  
 app.config['DEBUG'] = True
- 
+app.config['SECRET_KEY'] = '79c39690efaf2fd90101c64261bb95df427e226f6b976d86'
 from app import views  # noqa
